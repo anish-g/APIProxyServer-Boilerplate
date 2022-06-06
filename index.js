91efcbe7664aa5c0bfa3e3c863219856c29200b2
Anish Ghimire <anish.ghimire001@gmail.com>
2022-06-06 23:41:51 +0545

    Completed APIProxy Boilerplate

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const rateLimit = require('express-rate-limit')
+require('dotenv').config()
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 100
+})
+app.use(limiter)
+app.set('trust proxy', 1)
+
+// Set static folder
+// app.use(express.static('public'))
+
+// Routes
+app.use('/api', require('./routes'))
+
+// Enable cors
+app.use(cors())
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
